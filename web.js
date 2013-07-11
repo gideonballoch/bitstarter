33ce22f4d5961c91aba5f866d099bc41ba41c533
Gideon Balloch <gideonballoch@gmail.com>
2013-07-11 16:58:04 +0000

    updated web.js w output from index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
+var fs = require('fs');
+var input = "index.html";
+var output = fs.readFileSync(input).toString('utf-8');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send("Hello World 2");
+  response.send(output);
 });
 
 var port = process.env.PORT || 5000;
