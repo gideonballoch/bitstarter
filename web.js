300c262f3655e9b98ecf258c325de61c725c2571
Gideon Balloch <gideonballoch@gmail.com>
2013-07-11 04:09:59 +0000

    changed wbjs back to normal

--- web.js
+++ web.js
@@ -1,10 +1,9 @@
 var express = require('express');
-var fs = require('fs');
-var index = "index.html";
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(index.html);
+  response.send("Hello World 2");
 });
 
 var port = process.env.PORT || 5000;
