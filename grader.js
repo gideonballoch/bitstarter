1579bdb45949a55fe41c95e28d79f26d15ecb5ac
Gideon Balloch <gideonballoch@gmail.com>
2013-07-11 22:36:45 +0000

    altered grader.js but still have small error

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require ('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -46,6 +47,10 @@ var loadChecks = function(checksfile) {
 
 var checkHtmlFile = function(htmlfile, checksfile) {
     $ = cheerioHtmlFile(htmlfile);
+	return checkHtml($,checksfile);
+};
+
+var checkHtml = function($, checksfile) {
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -65,10 +70,26 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
+        .option('-u, --url <url>', 'Website URL' )
+	.parse(process.argv);
+
+	if ( program.url ) {
+		rest.get(program.url).on('complete',function(result){
+			if (result instanceof Error) {
+				console.log('Error: ' + result.message);
+				process.exit(1);
+			} else {
+				$ = cheerio.load(result);
+				var checkJson = checkHtml($, program.checks);
+				var outJson = JSON.stringify(checkJson, null, 4);
+				console.log(outJson);
+			}
+		});
+	} else {
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
+	};
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
